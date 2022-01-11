3c24239ca714b5d7d53d5f0e6b2f090f38501e04
bora001 <bora.vancouver@gmail.com>
2022-01-10 21:56:35 -0800

    card render

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 console.log("recipe");
+
+const bestRecipe = async function () {
+  try {
+    const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes?search=best"
+    );
+    const data = await res.json();
+    const recipes = data.data.recipes;
+    for (let recipe of recipes) {
+      cardRender(recipe);
+    }
+  } catch (err) {
+    alert(err);
+  }
+};
+
+const cardList = document.querySelector(".card_list");
+const cardRender = (data, title) => {
+  const buildCard = `
+      <div class="card_detail">
+
+    <a href="${data.id}">
+          <img
+          src="${data.image_url}"
+            alt="${data.title}"
+          />
+          <p>${data.title}</p>
+        </a>
+        </div>
+    `;
+
+  cardList.insertAdjacentHTML("afterbegin", buildCard);
+};
+
+bestRecipe();
